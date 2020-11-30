b05cf907816621e84e18667c5c979a7b8117c25b
Abhishek Sinha <absinha8@in.ibm.com>
2020-11-30 17:10:35 +0530

    hi

---
+++ build/lib/abhishek_package/test_class.py
@@ -0,0 +1,12 @@
+class test_class:
+
+    @staticmethod
+    def read_data():
+        """
+        This method reads data from snowflake
+        :param: spark
+        :return: dataframe
+        """
+        print("abhishek ahs made the package")
+
+    
